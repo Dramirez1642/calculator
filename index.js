a055154d72fdbf438f0b0583f938dfe9a3a128dd
Dramirez1642 <daltonramirez00@gmail.com>
2024-08-20 00:29:41 -0700

    Redid the beginning of the exercise in order.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Basic Math Operations
 
-const addition = function(a, b) {
+const add = function(a, b) {
     return a + b;
-};
+}
 
 const subtract = function(a, b) {
     return a - b;
@@ -18,33 +18,28 @@ const divide = function(a, b) {
 
 
 // Variables definition
-let num1 = 123;
+let num1 = 0;
 let num2;
 let operator;
 
-// Initial population of the display and creation of variable for further changes
-const currentNum = document.querySelector('.display')
 
-
-
-
-// Function to grab number and set numbers based on input
-const setNum = function(input) {
-    if (input == 'equals') {
-        if (operator === undefined) {
-            alert('There is no operator selected!');
-        }
-        else {
-            if (operator == 'addition'){ 
-
-            }
-        }
+// The operate function that will call the above functions
+const operate = function(a, b, operator) {
+    if (operator == 'add') {
+        return add(a, b);
+    }
+    else if (operator == 'subtract') {
+        return subtract(a, b);
+    }
+    else if (operator == 'multiply') {
+        return multiply(a, b);
+    }
+    else if (operator == 'divide') {
+        return divide(a, b);
     }
 }
 
+// Display code
 
-
-const button = document.querySelectorAll('button');
-
-button.addEventListener('click', e=> {});
+let display;
 
