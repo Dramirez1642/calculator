7f60c2beb37a5b5ff550379b227d66c104d72e57
Dramirez1642 <daltonramirez00@gmail.com>
2024-08-28 22:37:37 -0700

    Finished calculator project and it fully works except for the percent function.

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-// Basic Math Operations
-
-const add = function(a, b) {
-    return a + b;
-}
-
-const subtract = function(a, b) {
-    return a - b;
-}
-
-const multiply = function(a, b) {
-    return a*b;
-}
-
-const divide = function(a, b) {
-    return a/b;
-}
+(function() {
 
 
 // Variables definition
 let currentNum = '';
 let previousNum;
-let operator;
+let operator = '';
 
 let displayElem = document.querySelector('.display');
 displayElem.textContent = currentNum;
@@ -28,39 +12,86 @@ displayElem.textContent = currentNum;
 // The operate function that will call the above functions
 const operate = function(a, b, operator) {
     if (operator == 'add') {
-        return add(a, b);
+        return Number(a) + Number(b);
     }
     else if (operator == 'subtract') {
-        return subtract(a, b);
+        return Number(a) - Number(b);
     }
     else if (operator == 'multiply') {
-        return multiply(a, b);
+        return Number(a) * Number(b);
     }
     else if (operator == 'divide') {
-        return divide(a, b);
+        return Number(a) / Number(b);
     }
 }
 
 // Display code
 const populateDisplay = function(input) {
-    console.log(input)
-    if (input != '0') {
-        currentNum = currentNum + input;
+    //debugger
+    if (input == 'clear-everything') {
+        currentNum = '';
+        previousNum = '';
+        operator = '';
+    }
+    else if (input == 'clear-current') {
+        currentNum = '';
+    }
+    else if (input == 'equals') {
+        currentNum = operate(previousNum, currentNum, operator);
+        previousNum = '';
+        operator = ''
+    }
+    else if (input == '%') {
+        alert("Not implemented, it's just for the look. Go look up what this button actually does on the windows calculator and tell me you can explain what the fuck that is.")
+    }
+    else if (input == 'backspace'){
+        currentNum = currentNum.toString().slice(0, currentNum.toString().length - 1);
+    }
+    else if (input == 'square') {
+        currentNum = Number(currentNum) * Number(currentNum);     
+    }
+    else if (input == 'square-root') {
+        currentNum = Math.sqrt(Number(currentNum));
+    }
+    else if (input == 'reciprocal') {
+        currentNum = 1 / currentNum;
         displayElem.textContent = currentNum;
     }
-    else if (input == '0') {
-        if (currentNum != '') {
-            currentNum = currentNum + input;
-            displayElem.textContent = currentNum;
+    else if (input == 'decimal')  {
+        if (!currentNum.toString().includes('.')) {
+            currentNum = currentNum + '.';
         }
     }
+    else if (input == 'sign') {
+        currentNum = currentNum - 2*(currentNum)
+    }
+    else if (isNaN(Number(input)) && operator != "") {
+            alert("An operator has already been chosen! Press equals to finish your operation.")
+    }
+    else if (isNaN(Number(input)) && operator == "") {
+        operator = input;
+        previousNum = currentNum;
+        currentNum = '';
+    }
+    else if (input == '0' && currentNum == ''){
+        alert("You can't have leading zeros.")
+    }
+    else {
+        currentNum = currentNum + input;
+    }
+    displayElem.textContent = currentNum;
 }
+
 
 
 let buttons = document.querySelector('.buttons')
 buttons.addEventListener('click', function(e) {
     populateDisplay(e.target.id)
 })
+
+
+})()
+
 
 
 
